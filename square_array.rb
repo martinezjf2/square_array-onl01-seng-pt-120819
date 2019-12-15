a3ccff0564bb242744ba43cb6b1aaf0d3ccb86a1
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-15 20:33:22 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,11 @@
 
 def square_array(array)
+  new_numbers= []
 array.each do |number|
-new_numbers << number ** 2  
-  return square_array
+new_numbers.push (number ** 2)  
+  
 end
 
+new_numbers
 
 end
